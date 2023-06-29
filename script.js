80f675e56c628f36949a894af7af0909baaea010
Anderson Echavarria Blandon <AndersonEchavarria@comfama.com.co>
2023-06-28 22:41:39 -0500

    se realiza la transicion con js y css

---
+++ script.js
@@ -0,0 +1,10 @@
+function cambiar () {
+const izquierdo= document.getElementById('izquierdo')
+const derecho= document.getElementById('derecho')
+const iconos = document.getElementById('iconos')
+const boton = document.getElementById('boton')
+izquierdo.classList.add('izquierdo')
+derecho.classList.add('derecho')
+iconos.classList.add('iconos')
+boton.classList.add('boton')
+}
